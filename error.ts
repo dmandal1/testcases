991118bec3f06e6c9666424baafa0b78f4110158
Deepak Kumar Mandal <mdeepak.be16@gmail.com>
2025-03-13 17:47:51 +0530

    Update error.ts

--- error.ts
+++ error.ts
@@ -1,28 +1,7 @@
-Property 'mockResolvedValue' does not exist on type '(ian: string, request: any, source?: string) => Promise<QueueMessage>'.
+LabTestService > should processRequest successfully
+-----
+Error: A circular dependency has been detected inside RootTestModule. Please, make sure that each side of a bidirectional relationships are decorated with "forwardRef()".
 
-
-  Argument of type '{ request_body: {}; }' is not assignable to parameter of type 'IncomingRequestDto'.
-  Type '{ request_body: {}; }' is missing the following properties from type 'IncomingRequestDto': user, method, url, response_status, error_messagets
-
-Property 'mockRejectedValue' does not exist on type '(data: any) => Promise<void>'.
-
-  Argument of type '{ request_body: {}; }' is not assignable to parameter of type 'IncomingRequestDto'.
-  Type '{ request_body: {}; }' is missing the following properties from type 'IncomingRequestDto': user, method, url, response_status, error_message
-
-Property 'mockResolvedValue' does not exist on type '(queueMessage: any) => Promise<any>'.
-
-
-  Type 'number' is not assignable to type 'string'.ts(2322)
-queueMessage.entity.ts(14, 3): The expected type comes from property 'id' which is declared here on type 'QueueMessage'
-
-
-No overload matches this call.
-  Overload 1 of 4, '(object: LabTestService, method: never, accessType: "get"): SpyInstance<never, []>', gave the following error.
-    Argument of type '"logger"' is not assignable to parameter of type 'never'.
-  Overload 2 of 4, '(object: LabTestService, method: never, accessType: "set"): SpyInstance<void, [never]>', gave the following error.
-    Argument of type '"logger"' is not assignable to parameter of type 'never'.
-
-
-  Argument of type '{ log: jest.Mock<any, any>; }' is not assignable to parameter of type 'never'.
-
-  
+  LabTestService > should rollback transaction on error in processRequest
+-----
+Error: A circular dependency has been detected inside RootTestModule. Please, make sure that each side of a bidirectional relationships are decorated with "forwardRef()".
